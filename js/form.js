9f9de339e0a85c339d35afa387c5b06e4344c4d5
Igor Gomes <igorgomesdebrito@gmail.com>
2021-12-28 16:48:07 -0300

    melhorando o entendimento do nosso código

---
+++ js/form.js
@@ -0,0 +1,38 @@
+// incluindo no formulario
+var botaoCadastrar = document.querySelector("#cadastro-paciente");
+
+botaoCadastrar.addEventListener("click",function(event){
+    event.preventDefault();
+    // declarando as variáveis form para indicar a inclusão de novos pacientes a tabela
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    // para pegar os valores informados dentro dos inputs usamos o.value pra buscar esse valor
+
+    var CadPacienteTr = document.createElement("tr");
+
+    var CadnomeTd = document.createElement("td");
+    var CadpesoTd = document.createElement("td");
+    var CadAlturaTd = document.createElement("td");
+    var CadGorduraTd = document.createElement("td");
+    var CadImcTd = document.createElement("td");
+
+    CadnomeTd.textContent = nome;
+    CadpesoTd.textContent = peso;
+    CadAlturaTd.textContent = altura;
+    CadGorduraTd.textContent = gordura;
+    
+
+    CadPacienteTr.appendChild(CadnomeTd);
+    CadPacienteTr.appendChild(CadpesoTd);
+    CadPacienteTr.appendChild(CadAlturaTd);
+    CadPacienteTr.appendChild(CadGorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(CadPacienteTr);
+    
+});
